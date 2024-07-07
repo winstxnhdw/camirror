1773f2ebeaf7595604a9fb0a8fbd4fca1be9082f
winstxnhdw <winstxnhdw@gmail.com>
2024-07-07 19:56:21 +0100

    fix: service worker listeners cannot be async

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,13 +1,13 @@
 const cacheName = 'camirror'
 
-self.addEventListener('install', async (event) => {
-  const cache = await caches.open(cacheName)
-  event.waitUntil(cache.addAll(['/', '/index.js', '/index.html', '/index.css']))
-})
+self.addEventListener('install', (event) =>
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(['/', '/index.js', '/index.html', '/index.css'])),
+  ),
+)
 
 self.addEventListener('activate', (_) => console.log('Service Worker is ready'))
 
-self.addEventListener('fetch', async (event) => {
-  const response = await caches.match(event.request)
-  event.respondWith(response ?? fetch(event.request))
-})
+self.addEventListener('fetch', (event) =>
+  event.respondWith(caches.match(event.request).then((response) => response ?? fetch(event.request))),
+)
