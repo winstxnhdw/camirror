147f4cd202df13e4bfa9025c5cc0b4d5cc037aa2
winstxnhdw <winstxnhdw@gmail.com>
2024-07-07 19:16:23 +0100

    fix: consistently use async/await

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,18 +1,13 @@
 const cacheName = 'camirror'
 
-self.addEventListener('install', (event) =>
-  event.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(['/', '/index.js', '/index.html', '/index.css'])),
-  ),
-)
+self.addEventListener('install', async (event) => {
+  const cache = await caches.open(cacheName)
+  event.waitUntil(cache.addAll(['/', '/index.js', '/index.html', '/index.css']))
+})
+
+self.addEventListener('activate', (_) => console.log('Service Worker is ready'))
 
 self.addEventListener('fetch', async (event) => {
   const response = await caches.match(event.request)
-  event.respondWith(response || fetch(event.request))
-})
-
-self.addEventListener('activate', async (event) => {
-  const cacheNames = await caches.keys()
-  const promises = await Promise.all(cacheNames.map((name) => (name !== cacheName ? caches.delete(name) : undefined)))
-  event.waitUntil(promises)
+  event.respondWith(response ?? fetch(event.request))
 })
